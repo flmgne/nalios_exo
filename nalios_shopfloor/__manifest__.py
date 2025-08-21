dce791cc3894086db6678dd43bd81a90aefb1ca4
flmgne <florianmataigne@msn.com>
2025-08-21 14:26:16 +0200

    Save

---
+++ nalios_shopfloor/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "nalios Shopfloor",
+    "summary": """Custom module """,
+    "version": "17.0.0.0",
+    "author": "Nalios",
+    "license": "OEEL-1",
+    "depends": [
+        "mrp_workorder",
+    ],
+}
